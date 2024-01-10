1b2dbad65bd642f999ce6cbab337e3ee8b8d70de
Patrick Runkel <patrickrunkel86@gmail.com>
2024-01-10 16:54:50 +0100

    typewriter configured

--- js/derkochscript.js
+++ js/derkochscript.js
@@ -1,3 +1,37 @@
+//typewriter
+
+
+  document.addEventListener('DOMContentLoaded', function () {
+    const targetElement = document.getElementById('typewriter');
+    const textArray = ["Welcome to TaPa's Kitchen...", "Explore our delicious recipes", "Enjoy cooking with ease..."];
+    let textIndex = 0;
+    let charIndex = 0;
+
+    function type() {
+      if (charIndex < textArray[textIndex].length) {
+        targetElement.innerHTML += textArray[textIndex].charAt(charIndex);
+        charIndex++;
+        setTimeout(type, 100);
+      } else {
+        setTimeout(erase, 1500);
+      }
+    }
+
+    function erase() {
+      if (charIndex > 0) {
+        targetElement.innerHTML = textArray[textIndex].substring(0, charIndex - 1);
+        charIndex--;
+        setTimeout(erase, 50);
+      } else {
+        textIndex = (textIndex + 1) % textArray.length;
+        setTimeout(type, 500);
+      }
+    }
+
+    setTimeout(type, 500);
+  });
+//___________________________________________________________________________________
+
 "use strict";
 
 const inputEl = document.querySelector(".input-chat");
